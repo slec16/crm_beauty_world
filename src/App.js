c0fd5de1916789be41ed99b71be2f2f79e49078c
Alexandr Kopytov <sankopytov.76@mail.ru>
2023-02-02 21:34:27 +0700

    masters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes, Route, Link} from "react-router-dom";
+import './style.scss';
 
-function App() {
+import { MastersPage } from './pages/masters'
+import { LoginPage } from './pages/login';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<MastersPage />} />
+        <Route path="login" element={<LoginPage />} />
+      </Routes>
+
     </div>
   );
 }
-
-export default App;
